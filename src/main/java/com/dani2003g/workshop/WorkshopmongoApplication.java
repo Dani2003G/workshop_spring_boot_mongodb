9f2cbf5f7f940e8a412752d3deff3749d538492d
Daniel <daniel0202almeida@outlook.com>
2022-09-22 14:00:26 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/dani2003g/workshop/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.dani2003g.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dani2003g/workshop/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dani2003g.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
